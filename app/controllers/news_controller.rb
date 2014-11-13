4dbc7c1de1f4de63ccac17baf7e4216ff90348d2
Anowa <priscilla.hazel@meltwater.org>
2014-11-13 15:57:41 +0000

    added functions to my News Controller and created corresponding views

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,2 +1,35 @@
 class NewsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def create 
+    redirect_to(:action => 'index')
+  end
+
+  def edit
+  end
+  
+
+  def update
+    redirect_to(:action => 'show', :id => @patient.id)
+  end
+
+  def delete
+  end
+
+  def destroy
+    redirect_to(:action => 'index')  
+  end
+  
+
+  private
+  def news_params
+
+  end
 end
